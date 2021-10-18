6c9fc5fcd84030f95921d105523e2aeeb235e839
Artem Buhera <artem.bugera@gmail.com>
2021-10-18 14:23:14 +0200

    test web app

--- izbabot/main.py
+++ izbabot/main.py
@@ -3,11 +3,20 @@ from dotenv import load_dotenv
 
 from bot import bot
 from logger import setup_app_logger
+from quart import Quart
 
 
 def main():
     logger = setup_app_logger()
     logger.info('starting bot')
+
+    app = Quart(__name__)
+
+    @app.route('/')
+    def hello():
+        return 'Hello World!'
+
+    bot.loop.create_task(app.run_task())
 
     load_dotenv()
     bot_token = os.environ.get('bot_token')
